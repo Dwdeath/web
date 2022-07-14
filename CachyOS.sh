02405043ad915ffd490ba470f2a488fa277b1cfc
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-07-14 17:06:36 +0000

    Update CachyOS.sh
    enabled the script for testing

--- CachyOS.sh
+++ CachyOS.sh
@@ -1,7 +1,10 @@
+wget https://mirror.cachyos.org/cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz
+cd cachyos-repo
+sudo ./cachyos-repo.sh
 
-echo "skipping CachyOS"
-
-#wget https://mirror.cachyos.org/cachyos-repo.tar.xz
-#tar xvf cachyos-repo.tar.xz
-#cd cachyos-repo
-#sudo ./cachyos-repo.sh
+#echo "# cachyos repos" >> $PAC
+#echo "[cachyos-v3]" >> $PAC
+#echo "Include = /etc/pacman.d/cachyos-v3-mirrorlist" >> $PAC
+#echo "[cachyos]" >> $PAC
+#echo "Include = /etc/pacman.d/cachyos-mirrorlist" >> $PAC
