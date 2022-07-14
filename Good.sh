559097bf027ab46a766cb8889d10bb651131b4b0
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-07-14 17:30:15 +0000

    Update Good.sh
    disabled CachyOS for now.

--- Good.sh
+++ Good.sh
@@ -1,4 +1,4 @@
 #sh sh <(curl -s "")
 
 sh <(curl -s "https://raw.githubusercontent.com/Dwdeath/Chaotic/main/GarudaLinux.sh")
-sh <(curl -s "https://raw.githubusercontent.com/Dwdeath/Chaotic/main/CachyOS.sh")
+#sh <(curl -s "https://raw.githubusercontent.com/Dwdeath/Chaotic/main/CachyOS.sh")
